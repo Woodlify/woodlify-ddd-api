b51e469e564ac502f13a8be4b97b348015d734a0
price <nicole_14_2015@hotmail.com>
2022-06-06 12:04:33 -0500

    Revert "feat: Added Carpenter entity"
    This reverts commit 40cb24dcf00529950966c0d80afa122d96adbc8e.

--- src/users/domain/entities/carpenter.entity.ts
+++ src/users/domain/entities/carpenter.entity.ts
@@ -1,22 +1,3 @@
 import { UserId } from '../value-objects/user-id.value';
 import { Account } from '../value-objects/account.value';
 import { UserType } from '../enums/user-type.enum';
-import { CarpenterName } from '../../../common/domain/value-objects/carpenter-name.value';
-import { User } from './user.entity';
-
-export class Customer extends User {
-  private name: CarpenterName;
-  public constructor(name: CarpenterName, account: Account) {
-    super(UserType.CARPENTER, account);
-    this.name = name;
-  }
-  public getId(): UserId {
-    return this.id;
-  }
-  public getName(): CarpenterName {
-    return this.name;
-  }
-  public changeName(name: CarpenterName): void {
-    this.name = name;
-  }
-}
